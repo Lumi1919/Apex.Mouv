6425ed906d9d962fad68729781f30f0116af0ac9
Lumi1919 <shensanlumi@gmail.com>
2023-09-17 01:55:23 +0100

    'a'

--- Apex/settings.py
+++ Apex/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'Apex.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
 }
 
 
