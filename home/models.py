3961083c4f27cfaaa4b8dd3ff58fd5e9234fb2b7
Lumi1919 <shensanlumi@gmail.com>
2023-09-17 14:00:09 +0100

    a

--- home/models.py
+++ home/models.py
@@ -16,7 +16,7 @@ class Multimedia(models.Model):
     title = models.CharField(max_length=250, null=True, blank=True)
     body = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    content = models.CharField(max_length=250, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
